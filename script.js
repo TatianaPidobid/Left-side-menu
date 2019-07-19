456978c05c79c8ee4770f56904f65451992898fe
Tatiana <44233614+TatianaPidobid@users.noreply.github.com>
2019-07-19 19:26:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".burger").on("click", function () {
+    $(".fas, aside").toggleClass("show");
+})
+
+
+// const burger = document.querySelector(".burger");
+
+// const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-times");
+// const column = document.querySelector("aside");
+
+// burger.addEventListener("click", function () {
+//     iconBurger.classList.toggle("show");//yes
+//     iconX.classList.toggle("show");//no
+//     column.classList.toggle("show");//no
+
+// })
